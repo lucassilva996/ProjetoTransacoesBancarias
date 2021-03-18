05c4e943e8627f1b459ed530eecc50e5040af3ca
lucassilva996 <lucassilva996@outlook.com>
2021-03-18 02:09:46 -0300

    Projeto finalizado

--- Classes/Conta.cs
+++ Classes/Conta.cs
@@ -1,7 +1,15 @@
+using System;
+
 namespace Lucds.Bank
 {
     public class Conta
     {
+    
+        private TipoConta TipoConta {get; set;}
+        private double Saldo {get; set;}
+        private double Credito {get; set;}
+        private string Nome { get; set; }
+
         public Conta(TipoConta tipoConta, double saldo, double credito, string nome) 
         {
                 this.TipoConta = tipoConta;
@@ -9,11 +17,42 @@ namespace Lucds.Bank
                 this.Credito = credito;
                 this.Nome = nome;
         }
-        
-        private TipoConta TipoConta {get; set;}
-        private double Saldo {get; set;}
-        private double Credito {get; set;}
-        private string Nome { get; set; }
+
+        public bool Sacar(double valorSaque)
+        {
+            if (this.Saldo - valorSaque < (this.Credito *-1)){
+                Console.WriteLine("Saldo insuficiente!");
+                return false;
+            }
+            this.Saldo -= valorSaque;
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+
+            return true;
+        }
+
+        public void Depositar(double valorDeposito)
+        {
+            this.Saldo += valorDeposito;
+
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+        }
+
+        public void Transferir(double valorTransferencia, Conta contaDestino)
+        {
+            if(this.Sacar(valorTransferencia)){
+                contaDestino.Depositar(valorTransferencia);
+            }
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "TipoConta " + this.TipoConta + " | ";
+            retorno += "Nome " + this.Nome + " | ";
+            retorno += "Saldo " + this.Saldo + " | ";
+            retorno += "Credito " + this.Credito;
+            return retorno;
+        }
 
     }
 }
